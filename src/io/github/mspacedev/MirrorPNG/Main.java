ff2c833144c521d81f0531459d7ff0dd6ebdcace
MSpaceDev <mspacedev1@gmail.com>
2018-06-03 01:32:06 +0200

    Initial commit

---
+++ src/io/github/mspacedev/MirrorPNG/Main.java
@@ -0,0 +1,15 @@
+package io.github.mspacedev.MirrorPNG;
+
+import javax.swing.*;
+
+/**
+ * Copyright © MSpace-Dev 2018
+ * Do not distribute without proper permission from the author.
+ * https://mspace-dev.github.io
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "working!");
+    }
+}
